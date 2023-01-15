048db13be3527c8a15edee278e1969bcc68b08c2
Ivan-GoIT <educationzac@gmail.com>
2023-01-15 22:20:49 +0200

    task complete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,41 +1,47 @@
 import { FeedbackWidget } from '../FeedbackWidget/FeedbackWidget';
-import { useState, memo } from 'react';
-//import { useEffect } from 'react';
+import { useState, useCallback } from 'react';
+// import { useEffect } from 'react';
 
-export const App = memo(() => {
-  const [good, setGood] = useState({ name: 'Good', value: 0 });
-  const [neutral, setNeutral] = useState({ name: 'Neutral', value: 0 });
-  const [bad, setBad] = useState({ name: 'Bad', value: 0 });
+const useAppState = (key, value) => {
+  const [state, setState] = useState(() => ({ name: key, value }));
+
+  return [state, setState];
+};
+
+export const App = () => {
+  const [good, setGood] = useAppState('Good', 0);
+  const [neutral, setNeutral] = useAppState('Neutral', 0);
+  const [bad, setBad] = useAppState('Bad', 0);
+
+  const total = { name: 'Total', value: 0 };
+  const positivePercentage = {
+    name: 'Positive feedback',
+    value: 0,
+  };
 
   const handleStatisticChange = evt => {
     const { name } = evt.target;
+
+    const increaseValueByOne = state => ({ ...state, value: state.value + 1 });
+
     switch (name) {
       case 'good':
-        setGood({ ...good, value: good.value + 1 });
+        setGood(prev => increaseValueByOne(prev));
         break;
       case 'neutral':
-        setNeutral({ ...neutral, value: neutral.value + 1 });
+        setNeutral(prev => increaseValueByOne(prev));
         break;
       case 'bad':
-        setBad({ ...bad, value: bad.value + 1 });
+        setBad(prev => increaseValueByOne(prev));
         break;
       default:
         return;
     }
   };
 
-  const countTotalFeedback = () => {
-    return good.value + neutral.value + bad.value;
-  };
-  const countPositiveFeedbackPercentage = () => {
-    return Math.round((good.value * 100) / total.value.toFixed(5)) + '%';
-  };
-
-  const total = { name: 'Total', value: countTotalFeedback() };
-  const positivePercentage = {
-    name: 'Positive feedback',
-    value: countPositiveFeedbackPercentage(),
-  };
+  total.value = good.value + neutral.value + bad.value;
+  positivePercentage.value =
+    Math.round((good.value * 100) / total.value.toFixed(5)) + '%';
 
   return (
     <>
@@ -46,6 +52,6 @@ export const App = memo(() => {
       />
     </>
   );
-});
+};
 
 export default App;
--- src/components/FeedbackWidget/FeedbackButton/FeedbackButton.jsx
+++ src/components/FeedbackWidget/FeedbackButton/FeedbackButton.jsx
@@ -1,25 +1,29 @@
+import { memo } from 'react';
 import css from './FeedbackButton.module.css';
 import PropTypes from 'prop-types';
 
-export const FeedbackButton = props => {
-  const { buttonName, buttonTitle, onClick } = props;
-  return (
-    
-    <button
-    
-      type="button"
-      name={buttonName}
-      className={css.button}
-      onClick={onClick}
-    >
-      {console.log("FeedbackButton.render:",buttonTitle)}
-      {buttonTitle}
-    </button>
-  );
-};
+export const FeedbackButton = memo(
+  ({ buttonName, buttonTitle, onClick }) => {
+    return (
+      <button
+        type="button"
+        name={buttonName}
+        className={css.button}
+        onClick={onClick}
+      >
+        {console.log('FeedbackButton.render:', buttonTitle)}   
+        {buttonTitle}
+      </button>
+    );
+  },
+  (prev, next) =>
+    prev.buttonName === next.buttonName && prev.buttonTitle === next.buttonTitle
+);
+
+FeedbackButton.displayName = 'FeedbackButton';
 
 FeedbackButton.propTypes = {
   buttonName: PropTypes.string.isRequired,
   buttonTitle: PropTypes.string.isRequired,
-  onClick:PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
